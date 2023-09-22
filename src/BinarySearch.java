5b2a6a149e15b0bf4e03056b20e98d6abc7fb86c
AFAYYAD <fayyadahmad94@gmail.com>
2023-09-22 15:18:27 +0300

    Initial commit

---
+++ src/BinarySearch.java
@@ -0,0 +1,32 @@
+
+
+public class BinarySearch {
+
+    static int binarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int high = nums.length - 1;
+        int low = 0;
+
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                high = mid;
+            } else if (nums[mid] < target) {
+                low = mid;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int[] numsLists = { -1, 0, 3, 5, 9, 12 };
+        System.out.print(binarySearch(numsLists,5));
+
+    }
+}
